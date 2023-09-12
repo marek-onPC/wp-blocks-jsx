0a0540173c0bd74a0cef57938b6ee3491bcd9874
Marek Smieja <mareksmieja@gmail.com>
2023-09-12 20:31:42 +0900

    Update Icon - old Twitter to X

--- assets/scripts/blocks/socialShareButtonsBlock/edit.js
+++ assets/scripts/blocks/socialShareButtonsBlock/edit.js
@@ -1,5 +1,5 @@
 const { Fragment } = wp.element;
-const { Dashicon, ToggleControl } = wp.components;
+const { Dashicon, ToggleControl, Icon } = wp.components;
 
 /**
  * Edit function for Social Share Buttons block's Gutenberg Block Editor functionality
@@ -45,7 +45,13 @@ export const edit = (props) => {
           />
         </div>
         <div className="blocksplus-editor-social-share-buttons-block__share-button">
-          <Dashicon icon="twitter" className="blocksplus-editor-social-share-buttons-block__share-icon twitter" />
+          <span className="dashicon dashicons blocksplus-editor-social-share-buttons-block__share-icon twitter">
+            <Icon icon={
+              <svg width="24" height="34">
+                <path xmlns="http://www.w3.org/2000/svg" d="M 11.0892 7.8961 l 6.9366 -7.8961 h -1.6431 l -6.0256 6.8546 l -4.8091 -6.8546 h -5.5481 l 7.2738 10.3664 l -7.2738 8.2792 h 1.6431 l 6.359 -7.2402 l 5.0798 7.2402 h 5.5481 m -16.3938 -17.4321 h 2.5244 l 11.6208 16.2783 h -2.525" />
+              </svg>
+            } />
+          </span>
           <ToggleControl
             checked={attributes.twitter}
             onChange={() => {
